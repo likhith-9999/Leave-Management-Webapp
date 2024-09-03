73edd8d890dd27ba366ab11363da1dd2b312a94e
Likhith Ch <104769949+likhith-9999@users.noreply.github.com>
2024-09-03 09:52:07 +0530

    Delete src/main directory

--- src/main/java/com/wavemaker/leavemanagement/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.wavemaker.leavemanagement.repository;
-
-import java.sql.SQLException;
-
-public interface UserRepository {
-    boolean authenticate(String emailId, String password) throws SQLException, ClassNotFoundException;
-}
--- src/main/java/com/wavemaker/leavemanagement/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.wavemaker.leavemanagement.service;
-
-import java.sql.SQLException;
-
-public interface UserService {
-    boolean authenticate(String emailId, String password) throws SQLException, ClassNotFoundException;
-}
